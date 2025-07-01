45896d83fe78da7f9fe8dd95563ab7fa604e2269
Redneckayak <redneckayak@gmail.com>
2025-07-01 15:36:17 -0500

    Update app.py

--- app.py
+++ app.py
@@ -17,5 +17,5 @@ def whiff_watch():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 10000))  # Default to 10000 if PORT isn't set
+    port = int(os.environ.get('PORT', 10000))  # Default to 10000 if PORT not set
     app.run(debug=False, host='0.0.0.0', port=port)
